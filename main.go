527af1c4eabfa67a503c932c2810bb155909f2c2
cipepser <respepic@gmail.com>
2017-08-05 01:27:36 +0900

    use reconstrcut

--- main.go
+++ main.go
@@ -2,34 +2,40 @@ package main
 
 import (
 	"fmt"
-	"log"
 
 	"./hopscotch"
 )
 
 func main() {
 	h := hopscotch.NewHopscotch()
-
+	// h.Insert(1)
+	// h.Insert(2)
+	// h.Insert(3)
+	// h.Insert(4)
+	// h.Insert(5)
+	//
 	keys := make([]int64, hopscotch.N)
 	for k := 1; k <= int(hopscotch.N); k++ {
 		keys[k-1] = int64(k)
 	}
-	fmt.Println(keys)
+	// fmt.Println(keys)
 
 	for _, k := range keys {
 		err := h.Insert(k)
+		if err != nil {
+			// log.Fatal(err)
+			h = h.Reconstruct()
+		}
+
 		for i, b := range h {
 			fmt.Println(i, ":", b)
 		}
 		fmt.Println("~~~~~~~~~~~~")
-		if err != nil {
-			log.Fatal(err)
-		}
 	}
 
-	// fmt.Println(len(h))
-	// for i, b := range h {
-	// 	fmt.Println(i, ":", b)
-	// }
+	fmt.Println(len(h))
+	for i, b := range h {
+		fmt.Println(i, ":", b)
+	}
 
 }
